5031a20b161bb361ef3ce3a8d66b66c76eadc2a4
Dale Jung <dale@dalejung.com>
2016-07-27 14:08:57 -0400

    Fix binary files upload.

--- slackclient/_slackrequest.py
+++ slackclient/_slackrequest.py
@@ -21,12 +21,13 @@ class SlackRequest(object):
         '''
         post_data = post_data or {}
 
+        files = post_data.pop('files', None)
+
         for k, v in six.iteritems(post_data):
             if not isinstance(v, six.string_types):
                 post_data[k] = json.dumps(v)
 
         url = 'https://{0}/api/{1}'.format(domain, request)
         post_data['token'] = token
-        files = {'file': post_data.pop('file')} if 'file' in post_data else None
 
         return requests.post(url, data=post_data, files=files)
